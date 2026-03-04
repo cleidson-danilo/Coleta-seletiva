52db72b74ba03c737c79bc4d339d4403d6d759eb
Cleidson Danilo <cleidsondanilo18@gmail.com>
2026-03-03 21:08:22 -0300

    fazendo a ligação com o banco

--- src/config/db.js
+++ src/config/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '???', // Coloque a senha que você configurou no Workbench
-    database: 'aqui bota o nome do banco'
+    password: '85750076', // Coloque a senha que você configurou no Workbench
+    database: 'coleta_seletiva' // Coloque o nome do banco de dados que você criou (ex: coleta_seletiva)
 });
 
 connection.connect((err) => {
